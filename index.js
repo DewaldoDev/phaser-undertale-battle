5fed5a132cb2e03e5eb32f0eade48ea450f7b581
Tyler Dewald <dewaldodev@gmail.com>
2020-11-01 10:36:49 -0600

    Create global variables using this keyword

--- index.js
+++ index.js
@@ -18,30 +18,30 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
-var heart;
 
 function preload() {
   this.load.image("heart", "https://64.media.tumblr.com/avatar_6a16f384117d_128.pnj");
 }
 
 function create() {
-  heart = this.add.sprite(400, 300, "heart");
-  heart.displayHeight = 20;
-  heart.displayWidth = 20;
+  this.heart = this.add.sprite(400, 300, "heart");
+  this.heart.displayHeight = 20;
+  this.heart.displayWidth = 20;
+
+  this.keys = this.input.keyboard.createCursorKeys();
 }
 
 function update() {
-  const cursorKeys = this.input.keyboard.createCursorKeys();
-  if (cursorKeys.up.isDown) {
-    heart.y -= 4;
+  if (this.keys.up.isDown) {
+    this.heart.y -= 4;
   }  
-  if (cursorKeys.down.isDown) {
-    heart.y += 4;
+  if (this.keys.down.isDown) {
+    this.heart.y += 4;
   }
-  if (cursorKeys.right.isDown) {
-    heart.x += 4;
+  if (this.keys.right.isDown) {
+    this.heart.x += 4;
   }
-  if (cursorKeys.left.isDown) {
-    heart.x -= 4;
+  if (this.keys.left.isDown) {
+    this.heart.x -= 4;
   }
 }
